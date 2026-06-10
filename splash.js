4b280b3f1bff6a086bb71bd55c7d68534e83bde4
arikbar07-pixel <arik.bar07@gmail.com>
2026-06-10 23:54:50 +0300

    Extend MY HABITS hold time by 1.5s

--- splash.js
+++ splash.js
@@ -223,7 +223,7 @@
             setWord('GET YOUR SHIT DONE');
             phase = 1;
             advancing = false;
-          }, 400);
+          }, 1900);
         }
 
       } else if (phase === 1) {
